7c5b932d81559ba1afeea7302d075e316cab1bdd
Aubrey Gunn <agunn369@gmail.com>
2023-04-13 21:51:22 -0500

    edits in js files

--- Develop/Lib/cli.js
+++ Develop/Lib/cli.js
@@ -1,6 +1,34 @@
 //user prompts in this file
 const questions = [
     {
+        type: 'input',
+        name: 'text',
+        message: 'Please enter no more than three letters for your logo.',
+        validate: theInput => {
+            if(theInput){
+                //add code for the three chars exception
+                return true;
+            } else {
+                console.log('No more than three characters allowed');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Please enter a color for your logo.',
+        validate: theInput => {
+            if(theInput){
+                //add code for color
+                return true;
+            } else {
+                console.log('Please enter a color keyword OR hexadecimal number');
+                return false;
+            }
+        }
+    },
+    {
         type: 'list',
         name: 'shape',
         message: 'Choose a shape',
@@ -15,17 +43,4 @@ const questions = [
             }
         }
     },
-    {
-        type: 'input',
-        name: 'text',
-        message: 'What text do you want in your image?',
-        validate: theInput => {
-            if(theInput){
-                return true;
-            } else {
-                console.log('Please add text to your image');
-                return false;
-            }
-        }
-    }
 ]
--- Develop/index.js
+++ Develop/index.js
@@ -2,9 +2,12 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const shapes = require('./Lib/shapes.js');
+const prompts = require('./Lib/cli.js');
+const newFile = require('./Lib/svg.js');
 
 //moved questions array to cli.js 
-    //cli used to store prompts, make sure to link all files so they can chat
+
+//cli used to store prompts, make sure to link all files so they can chat
 
 // moved writefile to svg.js
 
@@ -15,22 +18,3 @@ function init(){
 
 init();
 
-/*let svg = ""
-svg += '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
-svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
-svg += '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">\n'
-
-for (let x = 0; x < 12; x++) {
-    svg += `\t<circle cx="${centerX+(x*((radius*2)+5))}" cy="${centerY}" r="${radius}" style="${style}"/>\n`
-}
-
-
-svg += '</svg>'
-
-fs.writeFile('testSVG.svg', svg, (err) => {  
-    // throws an error, you could also catch it here
-    if (err) throw err;
-
-    // success case, the file was saved
-    console.log('SVG written!');
-});*/
