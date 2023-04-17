c3d231ed12799f8fcd1229a6c7023fedb358f228
Aubrey Gunn <agunn369@gmail.com>
2023-04-16 22:19:10 -0500

    added code for prompts and hex color

--- Develop/Lib/shapes.js
+++ Develop/Lib/shapes.js
@@ -1,17 +1,16 @@
 const fs = require('fs');
    
    class Shapes {
-     constructor(text, color, bkgndColor, border) {
+     constructor(text, color, shapeColor) {
        this.text = text;
        this.color = color;
-       this.bkgndColor = bkgndColor;
-       this.border = border;
+       this.shapeColor = shapeColor;
      }
    }
    
    class Circle extends Shapes {
-     constructor(text, color, bkgndColor, border) {
-       super(text, color, bkgndColor, border);
+     constructor(text, color, bkgndColor) {
+       super(text, color, bkgndColor);
      }
    
      renderSvg(path) {
@@ -19,17 +18,17 @@ const fs = require('fs');
        width="300" height="200"
        xmlns="http://www.w3.org/2000/svg">
    
-      <circle cx="150" cy="100" r="80" ${this.border ? `stroke-width="3" stroke="${this.color}" ` : ''}
-      fill="${this.bkgndColor}" />
+      <circle cx="150" cy="100" r="80"
+      fill="${this.shapeColor}" />
    
       <text x="150" y="120" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>
    
     </svg>`;
    
-       return new Promise((res, reject) => {
+       return new Promise((res, rej) => {
         fs.writeFile(path, circle, (err) => {
             if (err) {
-                reject(err);
+                rej(err);
             } else {
                 res();           
             }         
@@ -47,17 +46,17 @@ class Square extends Shapes{
         width="300" height="200"
         xmlns="http://www.w3.org/2000/svg">
     
-       <rect x="50" width="200" height="200" ${this.border ? `stroke-width="3" stroke="${this.color}" ` : ''}
-       fill="${this.bkgndColor}" />
+       <rect x="50" width="200" height="200" 
+       fill="${this.shapeColor}" />
     
        <text x="150" y="120" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>
     
      </svg>`;
     
-        return new Promise((res, reject) => {
+        return new Promise((res, rej) => {
          fs.writeFile(path, square, (err) => {
              if (err) {
-                 reject(err);
+                 rej(err);
              } else {
                  res();           
              }         
@@ -66,25 +65,25 @@ class Square extends Shapes{
  }
 }
 class Triangle extends Shapes{
-    constructor(text, color, bkgndColor, border) {
-        super(text, color, bkgndColor, border);
+    constructor(text, color, bkgndColor) {
+        super(text, color, bkgndColor);
     }
-    renderSVG(path) {
+    renderSvg(path) {
         const triangle = `<svg version="1.1"
         width="300" height="200"
         xmlns="http://www.w3.org/2000/svg">
     
-       <path d="M 150 30 L 270 170 L 30 170 Z" ${this.border ? `stroke-width="3" stroke="${this.color}" ` : ''}
-       fill="${this.bkgndColor}" />
+       <path d="M 150 30 L 270 170 L 30 170 Z"
+       fill="${this.shapeColor}" />
     
        <text x="150" y="150" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text>
     
      </svg>`;
     
-        return new Promise((res, reject) => {
+        return new Promise((res, rej) => {
          fs.writeFile(path, triangle, (err) => {
              if (err) {
-                 reject(err);
+                 rej(err);
              } else {
                  res();           
              }         
--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,8 @@
 // packages
 const fs = require('fs');
 const inquirer = require('inquirer');
+const hexColor = require('color-string');
 const shape = require('./Lib/shapes.js');
-const prompts = require('./Lib/cli.js');
-const logo = require('./Lib/svg.js');
 
 const questions = [
     {
@@ -11,27 +10,29 @@ const questions = [
         name: 'text',
         message: 'Please enter no more than three letters for your logo.',
         validate: theInput => {
-            if(theInput == 'bsh'){
-                //add code for the three chars exception(see if == works)
+            if(/^[a-zA-Z0-9]+$/.test(theInput) && theInput.length <= 3 && theInput.length >= 1){
+                //valid chars, test and length between 1 and 3
                 return true;
             } else {
-                console.log('No more than three characters allowed');
-                return false;
+                return 'Only three characters allowed per logo.';
             }
         }
     },
     {
         type: 'input',
         name: 'color',
-        message: 'Please enter a color for your logo.',
+        message: 'Please enter a text color for your logo.',
         validate: theInput => {
-            if(theInput){
-                //add code for color
+            if (/^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(theInput)) {
                 return true;
-            } else {
-                console.log('Please enter a color keyword OR hexadecimal number');
-                return false;
-            }
+              } else if (/^[a-zA-Z]+$/.test(theInput)) {
+                const colorObj = colorString.get(theInput);
+                if (colorObj !== null) {
+                    return true;
+                }
+              } else {
+                return 'Please enter a valid color name OR hex code.';
+              }
         }
     },
     {
@@ -44,11 +45,25 @@ const questions = [
             if (theInput){
                 return true;
             } else {
-                console.log('Please choose a shape');
-                return false;
+                return 'Please choose a shape for your logo.';
             }
         }
     },
+    {
+        type: 'input',
+        name: 'color',
+        message: 'Please enter shape color for your logo.',
+        validate: theInput => {
+            if (/^[a-zA-Z]+$/.test(theInput) || /^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/.test(theInput)) {
+                const colorObj = colorString.get(theInput);
+                if (colorObj !== null) {
+                    return true;
+                } else {
+                    return 'Please enter a valid color name OR hex-code.';
+                }
+              }
+        }
+    }
 ]
 
 //write to svg file
@@ -57,7 +72,7 @@ const writeFile = data => {
         if(err) {
             console.log(err);
         } else {
-            console.log('Success!')
+            console.log('Generated logo.svg');
         }
     })
 };
@@ -84,26 +99,6 @@ init();
 //THEN I can enter a color keyword (OR a hexadecimal number)(DONE)
 //WHEN I have entered input for all the prompts
 //THEN an SVG file is created named `logo.svg`
-//AND the output text "Generated logo.svg" is printed in the command line
+//AND the output text "Generated logo.svg" is printed in the command line(DONE)
 //WHEN I open the `logo.svg` file in a browser
-//THEN I am shown a 300x200 pixel image that matches the criteria I entered
-
-/*let svg = ""
-svg += '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
-svg += '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n'
-svg += '<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">\n'
-
-for (let x = 0; x < 12; x++) {
-    svg += `\t<circle cx="${centerX+(x*((radius*2)+5))}" cy="${centerY}" r="${radius}" style="${style}"/>\n`
-}
-
-
-svg += '</svg>'
-
-fs.writeFile('testSVG.svg', svg, (err) => {  
-    // throws an error, you could also catch it here
-    if (err) throw err;
-
-    // success case, the file was saved
-    console.log('SVG written!');
-});*/
+//THEN I am shown a 300x200 pixel image that matches the criteria I entered()
