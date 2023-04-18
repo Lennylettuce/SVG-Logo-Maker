382b9e7203229fb040b0b95341f8577591390cba
Aubrey Gunn <agunn369@gmail.com>
2023-04-17 20:37:57 -0500

    add tests

--- Develop/Lib/Tests/circle.test.js
+++ Develop/Lib/Tests/circle.test.js
@@ -2,13 +2,13 @@ const Circle = require('../circle.js');
 
 describe('Circle', () => {
     it('should return a blue circle with white text', () => {
-        const circle = new Circle('blue','EKO','white');
+        const circle = new Circle('blue','AUB','white');
         expect(circle.render()).toEqual(`
         <svg version="1.1"
             width="300" height="200"
             xmlns="http://www.w3.org/2000/svg">
             <circle cx="150" cy="100" r="100" fill="blue" />
-            <text x="150" y="125" font-size="70" text-anchor="middle" fill="white">EKO</text>
+            <text x="150" y="125" font-size="70" text-anchor="middle" fill="white">AUB</text>
         </svg>
         `
         );
--- Develop/Lib/Tests/square.test.js
+++ Develop/Lib/Tests/square.test.js
@@ -2,13 +2,13 @@ const Square = require('../square.js');
 
 describe('Square', () => {
     it('should return square that is red with yellow text', () => {
-        const square = new Square('red','EKO','yellow');
+        const square = new Square('red','AUB','yellow');
         expect(square.render()).toEqual(`
         <svg version="1.1"
             width="300" height="200"
             xmlns="http://www.w3.org/2000/svg">
             <rect width="200" height="200" fill="red"/>
-            <text x="100" y="125" font-size="70" text-anchor="middle" fill="yellow">EKO</text>
+            <text x="100" y="125" font-size="70" text-anchor="middle" fill="yellow">AUB</text>
         </svg>
         `
         );
