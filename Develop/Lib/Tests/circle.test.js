b04c5237d0657c7171030d1a6634c4b51a2b961d
Aubrey Gunn <agunn369@gmail.com>
2023-04-17 20:28:18 -0500

    added tests

---
+++ Develop/Lib/Tests/circle.test.js
@@ -0,0 +1,16 @@
+const Circle = require('../circle.js');
+
+describe('Circle', () => {
+    it('should return a blue circle with white text', () => {
+        const circle = new Circle('blue','EKO','white');
+        expect(circle.render()).toEqual(`
+        <svg version="1.1"
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            <circle cx="150" cy="100" r="100" fill="blue" />
+            <text x="150" y="125" font-size="70" text-anchor="middle" fill="white">EKO</text>
+        </svg>
+        `
+        );
+    });
+});
